801a35a3a55b943afcf1b7c7f3706196c5324a92
kws <89932020+katie-ws@users.noreply.github.com>
2021-11-05 19:08:29 +0000

    NewQuizForm - add quiz

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addQuizId } from "../topics/topicsSlice";
 
 export const quizzesSlice = createSlice({
     name: 'quizzes',
@@ -12,6 +13,14 @@ export const quizzesSlice = createSlice({
         }
     },   
 });
+
+export const addQuizForTopicId = (quiz) => {
+    const { topicId, id } = quiz;
+    return (dispatch) => {
+        dispatch(quizzesSlice.actions.addQuiz(quiz));
+        dispatch(addQuizId({ topicId: topicId, quizId: id }));
+    }
+}
 
 export const { addQuiz} = quizzesSlice.actions;
 export const selectQuizzes = (state) => state.quizzes.quizzes;
